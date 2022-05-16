c3ed7f40197c91349769acfb1d224849aa554e26
sadettin uysal <tubasariz@gmail.com>
2022-05-15 22:31:21 -0400

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args){
         //write your code
         System.out.println("I'm great!!");
+        System.out.println("I'm great2!!");
+        System.out.println("I'm great3!!");
     }
 }
